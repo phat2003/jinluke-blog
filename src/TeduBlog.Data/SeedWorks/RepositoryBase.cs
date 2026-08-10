a2860e486807470e4c2c106ec9d74591381105da
phat2003 <phamanhtienphat@gmail.com>
2026-08-10 22:36:36 +0700

    2.10. Create post repository and register services batch

--- src/TeduBlog.Data/SeedWorks/RepositoryBase.cs
+++ src/TeduBlog.Data/SeedWorks/RepositoryBase.cs
@@ -9,8 +9,8 @@ namespace TeduBlog.Data.SeedWorks
         private readonly DbSet<T> _dbSet;
         protected readonly TeduBlogContext _context;
         public RepositoryBase(TeduBlogContext context) { 
-            _context = context;
             _dbSet = context.Set<T>();
+            _context = context;
         }
         public void Add(T entity)
         {
