b95dd29433a51b40ed00fb730379af10e01e25b6
phat2003 <phamanhtienphat@gmail.com>
2026-08-26 15:40:01 +0700

    2.11. Add paging and mapper

--- src/TeduBlog.Core/Models/Content/PostDto.cs
+++ src/TeduBlog.Core/Models/Content/PostDto.cs
@@ -1,11 +1,4 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TeduBlog.Core.Domain.Content;
 
 namespace TeduBlog.Core.Models.Content
